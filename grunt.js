fe0cc9420826ac7a8033ac2a28cf4ac347b246b2
Blake Haswell <haswell00@gmail.com>
2014-04-09 11:47:12 +1000

    Converted tabs to spaces so that the project uses a consistant style.

--- grunt.js
+++ grunt.js
@@ -1,39 +1,39 @@
 /*globals module */
 
 module.exports = function(grunt) {
-	
-	/**
-	 * Define our Grunt configuration.
-	 */
-	grunt.initConfig({
-		
-		/**
-		 * Define the validation options to apply.
-		 * See: http://www.jshint.com/docs/
-		 */
-		jshint: {
-			options: {
-				browser: true,
-				curly: true,
-				eqnull: true,
-				immed: true,
-				jquery: true,
-				newcap: true,
-				noarg: true,
-				smarttabs: true,
-				sub: true,
-				undef: true,
-				unused: true
-			}
-		},
-		
-		/**
-		 * Define the files we'll validate.
-		 */
-		lint: {
-			all: ['grunt.js',
+
+    /**
+     * Define our Grunt configuration.
+     */
+    grunt.initConfig({
+
+        /**
+         * Define the validation options to apply.
+         * See: http://www.jshint.com/docs/
+         */
+        jshint: {
+            options: {
+                browser: true,
+                curly: true,
+                eqnull: true,
+                immed: true,
+                jquery: true,
+                newcap: true,
+                noarg: true,
+                smarttabs: true,
+                sub: true,
+                undef: true,
+                unused: true
+            }
+        },
+
+        /**
+         * Define the files we'll validate.
+         */
+        lint: {
+            all: ['grunt.js',
                 'src/jquery.txtTruncate.js']
-		},
+        },
 
         /**
          * Compress the plugin.
@@ -44,18 +44,18 @@ module.exports = function(grunt) {
                 dest: 'src/jquery.txtTruncate.min.js'
             }
         },
-		
-		/**
-		 * Allow Grunt to run by itself.
-		 */
-		watch: {
-			scripts: {
-				files: '<config:lint.all>',
-				tasks: 'lint'
-			}
-		}
-	});
-	
-	// By default, just run lint.
-	grunt.registerTask('default', 'lint');
+
+        /**
+         * Allow Grunt to run by itself.
+         */
+        watch: {
+            scripts: {
+                files: '<config:lint.all>',
+                tasks: 'lint'
+            }
+        }
+    });
+
+    // By default, just run lint.
+    grunt.registerTask('default', 'lint');
 };
